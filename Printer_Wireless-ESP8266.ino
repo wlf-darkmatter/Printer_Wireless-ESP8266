a774fb66e6a30fcbf4198737943d6ae98724c608
wlf-darkmatter <wlf_x_contion@sina.com>
2020-07-16 23:36:44 +0800

    实现了基本的串口通讯交互

--- Printer_Wireless-ESP8266.ino
+++ Printer_Wireless-ESP8266.ino
@@ -4,18 +4,23 @@
  Author:	wlf_x
 */
 #include <ESP8266WiFi.h>
-
+//ESP8266模块上电时，默认打印波特率为74880
+//固件中通信与串口默认为115200
 const char* ssid = "东南沿海王大哥";
 const char* pass = "19981213";
 
 void setup() {
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);//设置WiFi模式
-    WiFi.localIP();
+    WiFi.begin(ssid, pass);//连接路由器
+    Serial.print("正在连接到");
+    Serial.print(ssid);
+    Serial.print("\rOK\r");
     
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+    Serial.print("\rOK\r");
+    delay(100);
 }
