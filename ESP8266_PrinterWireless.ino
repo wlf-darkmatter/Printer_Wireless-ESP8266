b041905f1f7423e7112316cc36e815a5c34f738c
wlf-darkmatter <wlf_x_contion@sina.com>
2020-07-16 18:14:50 +0800

    基本文件

---
+++ ESP8266_PrinterWireless.ino
@@ -0,0 +1,19 @@
+/*
+ Name:		ESP8266_PrinterWireless.ino
+ Created:	2020/7/16 18:03:27
+ Author:	wlf_x
+*/
+#include <ESP8266WiFi.h>
+
+const char* ssid = "东南沿海王大哥";
+const char* pass = "19981213";
+
+void setup() {
+    WiFi.mode(WIFI_AP_STA);//设置WiFi模式
+    WiFi.softAP(ssid, pass);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
